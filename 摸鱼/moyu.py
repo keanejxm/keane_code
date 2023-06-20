08c8cc180a98d899502fa0a664421b6df1409de0
keane <jinxiaoming215@aliyun.com>
2023-06-20 15:16:15 +0800

    111

--- 摸鱼/moyu.py
+++ 摸鱼/moyu.py
@@ -8,11 +8,19 @@
 """
 
 import requests
+from lxml import etree
 
-url = "https://m.aishangba.info/122_122597/664056.html"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
-}
+url = "https://m.aishangba.info/122_122597/667005.html"
+while 1:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+    }
 
-res = requests.get(url,headers = headers)
-print(res)
+    res = requests.get(url, headers=headers)
+    if res.status_code == requests.codes.ok:
+        res_html = etree.HTML(res.text)
+        title = res_html.xpath("//span[@class = 'title']/text()")
+        next_url = res_html.xpath("//a[@id = 'pt_next']/@href")
+        url = f"https://m.aishangba.info{next_url[0]}"
+        print(title,url)
+    print(res)
