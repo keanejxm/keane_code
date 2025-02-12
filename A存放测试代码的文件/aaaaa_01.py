cc807e5e35b61f8f2d9bcd7c739d453dee2e7b3a
keane <jinxiaoming215@aliyun.com>
2025-02-12 18:01:15 +0800

    下载中国日历

--- A存放测试代码的文件/aaaaa_01.py
+++ A存放测试代码的文件/aaaaa_01.py
@@ -6,37 +6,22 @@
 :time  2025/1/26 13:54
 :desc  
 """
-import os.path
+import numpy as np
+import sklearn.linear_model as lr
+import sklearn.metrics as sm
+import sklearn.model_selection as ms
+import sklearn.pipeline as pp
+import sklearn.preprocessing as sp
+import pickle as pkl
 
-# if not os.path.exists("aaaa"):
-#     os.mkdir("aaaa")
-
-# import cv2
-#
-# img1 = cv2.imread("1df58719a.webp")
-# img2 = cv2.imread("d401d55fc.webp")
-# add = cv2.add(img1,img2)
-# cv2.imshow("image",add)
-# cv2.imshow("image2",img2)
-# cv2.imshow("iamge1",img1)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-import time
-
-# print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(1738830399)))
-# print(time.mktime(time.strptime("20250206", "%Y%m%d")))
-# b = time.strftime("%Y%m%d", time.localtime(time.mktime(time.strptime("20250206", "%Y%m%d")) - 1 * 24 * 60 * 60))
-# print(b)
-import cv2
-
-img1 = cv2.imread(r"E:\keane_python\github\keane_code\A存放测试代码的文件\户型图.jpg")
-cv2.imshow("img1", img1)
-flip = cv2.flip(img1, 1)
-cv2.imshow("img2", flip)
-cv2.waitKey()
-
-# cv2.imshow("img1",img1)
-# cv2.waitKey()
-cv2.destroyAllWindows()
-
-
+# pp.make_pipeline(sp.PolynomialFeatures(2), lr.LinearRegression())
+data1 = np.array([[1,2],[3,4]])
+data2 = np.array([[5,6]])
+mms = sp.MinMaxScaler()
+data = mms.fit(data1)
+data1_r = mms.transform(data1)
+data2_r = mms.transform(data2)
+print(data1_r)
+print(data2_r)
+# mms.transform([1,2,3,4,5])
+# lr.LinearRegression()
