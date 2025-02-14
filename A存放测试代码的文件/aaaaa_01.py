bcc82cc9549f978d61a016fcef68aa095b6c7dbe
keane <jinxiaoming215@aliyun.com>
2025-02-14 18:02:20 +0800

    下载中国日历

--- A存放测试代码的文件/aaaaa_01.py
+++ A存放测试代码的文件/aaaaa_01.py
@@ -6,10 +6,12 @@
 :time  2025/1/26 13:54
 :desc  
 """
+import os.path
 
 import numpy as np
 import pandas as pd
 import pickle as pkl
+import matplotlib.pyplot as plt
 
 import sklearn
 import sklearn.linear_model as lr
@@ -21,36 +23,20 @@ import sklearn.tree as st
 import sklearn.ensemble as se
 import sklearn.datasets as ds
 
-# 下载数据
-data = pd.read_excel("boston_data.xlsx")
-# 划分特征和真实值
-x = data.iloc[:, :-1]
-y = data.iloc[:, -1]
-# 划分训练集，测试集
-train_x, test_x, train_y, test_y = sklearn.model_selection.train_test_split(
-    x, y,
-    test_size=0.2,
-    random_state=42
-)
+x = np.array([[3, 1], [2, 5], [1, 8], [6, 4], [5, 2], [3, 5], [4, 7], [4, -1]])
+y = np.array([0, 1, 1, 0, 0, 1, 1, 0])
+
 # 构建模型
-# tree_model = st.DecisionTreeRegressor()
-tree_model = se.AdaBoostRegressor(
-    st.DecisionTreeRegressor(max_depth=7),
-    n_estimators=400,
-    random_state=7
-)
-# tree_model = se.RandomForestRegressor(n_estimators=400,max_depth=15,random_state=42)
-# tree_model = se.GradientBoostingRegressor(
-#     n_estimators=100,
-#     max_depth=10,
-#     random_state=42,
-# )
-# 进行训练
-tree_model.fit(train_x, train_y)
-# 模型预测
-pred_y = tree_model.predict(test_x)
-feature = tree_model.feature_importances_
-feature_name = tree_model.feature_names_in_
-r2_score = sm.r2_score(test_y, pred_y)
-print(r2_score)
-# 保存模型
+model = lr.LogisticRegression()
+# 训练模型
+model.fit(x, y)
+
+# 预测
+pred_y = model.predict(np.array([[3, 9], [6, 1]]))
+print(pred_y)
+
+a = x[:, 0]
+b = x[:, 1]
+
+plt.scatter(x[:, 0], x[:, 1])
+plt.show()
