2249e188f807303ffa44cd7e734bb42b7ccb822d
keane <jinxiaoming215@aliyun.com>
2025-02-24 17:22:15 +0800

    plt相关

--- A存放测试代码的文件/aaaaa_01.py
+++ A存放测试代码的文件/aaaaa_01.py
@@ -6,56 +6,22 @@
 :time  2025/1/26 13:54
 :desc  
 """
-import math
-import os.path
-
-import numpy as np
-import pandas as pd
-import pickle as pkl
+import cv2
+import os
 import matplotlib.pyplot as plt
 
-import sklearn
-import sklearn.linear_model as lr
-import sklearn.metrics as sm
-import sklearn.model_selection as ms
-import sklearn.pipeline as pp
-import sklearn.preprocessing as sp
-import sklearn.tree as st
-import sklearn.ensemble as se
-import sklearn.datasets as ds
-import sklearn.svm as svm
+data_dir = r"E:\keane_data\img_data"
 
-# 分类算法 逻辑回归、决策树、svm（支持向量机）、朴素贝叶斯
-# 读取数据
-files_dir = r"E:\keane_data\01_机器学习数据"
-df_data = pd.read_csv(os.path.join(files_dir, "multiple2.txt"), header=None)
-x = df_data.iloc[:, 0:-1]
-y = df_data.iloc[:, -1]
+img = cv2.imread(os.path.join(data_dir, r"1.png"), flags=1)
+img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
 
-# x_train, x_test, y_train, y_test = ms.train_test_split(x, y, test_size=0.2)
+img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
 
-model = svm.SVC(kernel='linear', C=1, gamma=1)
+bgr_img = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
 
-model.fit(x, y)
-
-# y_pred = model.predict(x_train)
-
-c1, c2 = (y == 0), (y == 1)
-x_array = np.array(x)
-l, r, h = x_array[:, 0].min() - 1, x_array[:, 0].max() + 1, 0.005
-b, t, v = x_array[:, 1].min() - 1, x_array[:, 1].max() + 1, 0.005
-grid_x = np.meshgrid(np.arange(l, r, h), np.arange(b, t, v))
-a_d = grid_x[0].ravel()
-flat_x = np.c_[grid_x[0].ravel(), grid_x[1].ravel()]
-flat_y = model.predict(flat_x)
-grid_y = flat_y.reshape(grid_x[0].shape)
-plt.pcolormesh(grid_x[0],grid_x[1],grid_y,cmap="gray")
-
-# plt.figure("svm png")
-# plt.xlabel("x")
-# plt.ylabel("y")
-# plt.scatter(x_array[(y==0)][:,0], x_array[(y==0)][:,1],c="orangered")
-# plt.scatter(x_array[(y==1)][:,0], x_array[(y==1)][:,1],c="green")
-
+cv2.imshow("bgr", bgr_img)
+cv2.imshow("img",img)
 
 plt.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
